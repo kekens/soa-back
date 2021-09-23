c2fbf3bdf0716a0ba06ca2b630b717b01e5380c4
kirill <kirillbobryakov82@gmail.com>
2021-09-23 14:54:44 +0300

    json data test

--- src/main/java/com/kekens/soa_lab_1/model/Coordinates.java
+++ src/main/java/com/kekens/soa_lab_1/model/Coordinates.java
@@ -1,11 +1,14 @@
 package com.kekens.soa_lab_1.model;
 
+import lombok.Data;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Data
 public class Coordinates {
     private long x;
     private int y; //Значение поля должно быть больше -895
--- src/main/java/com/kekens/soa_lab_1/servlet/LabWorkServlet.java
+++ src/main/java/com/kekens/soa_lab_1/servlet/LabWorkServlet.java
@@ -1,35 +1,74 @@
 package com.kekens.soa_lab_1.servlet;
 
+import com.google.gson.Gson;
+import com.kekens.soa_lab_1.dao.LabWorkDao;
+import com.kekens.soa_lab_1.model.Discipline;
+import com.kekens.soa_lab_1.model.LabWork;
+import com.kekens.soa_lab_1.service.LabWorkService;
+import com.kekens.soa_lab_1.service.impl.LabWorkServiceImpl;
 import com.kekens.soa_lab_1.util.HibernateSessionFactoryUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.logging.Logger;
 
-@WebServlet(value = "/")
+@WebServlet(value = "/lab-work")
 public class LabWorkServlet extends HttpServlet {
 
+    private final Gson gson = new Gson();
+    private final LabWorkService labWorkService = new LabWorkServiceImpl();
+
+    static Logger log = Logger.getLogger(LabWorkServlet.class.getName());
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-//        HibernateSessionFactoryUtil.getSessionFactory().openSession();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idString = request.getParameter("id");
+        LabWork labWork = labWorkService.findLabWorkById(Integer.parseInt(idString));
+        String labWorkJsonString = gson.toJson(labWork);
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(labWorkJsonString);
+        out.flush();
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        log.info("PUT");
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+        log.info(sb.toString());
+
+        LabWork labWork = gson.fromJson(sb.toString(), LabWork.class);
+        log.info(String.valueOf(labWork.getCoordinates().getX()));
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
