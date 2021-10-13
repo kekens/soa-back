0fe3fe03434dd7572eae982adae260d9dc96fbf2
kirill <kirillbobryakov82@gmail.com>
2021-10-13 12:17:54 +0300

    refactor, fix: change methods in LabWork servlet
    combine two methods to send a response into one
    
    change the status codes to more appropriate, the method PUT takes an ID from a URI, not body

--- src/main/java/com/kekens/soa_lab_1/servlet/LabWorkServlet.java
+++ src/main/java/com/kekens/soa_lab_1/servlet/LabWorkServlet.java
@@ -14,9 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @WebServlet(value = "/lab-work/*")
 public class LabWorkServlet extends HttpServlet {
@@ -34,54 +34,42 @@ public class LabWorkServlet extends HttpServlet {
 
         String path = request.getPathInfo();
 
-        if (path == null) {
-            // Filter
-            LabWorkFilterConfiguration labWorkFilterConfiguration = parseFilterRequest(request);
-            List<LabWork> listLabWork = labWorkService.findAllLabWorks(labWorkFilterConfiguration);
+        if ((path == null) || (path.equals("/"))) {
+            try {
+                // Filter
+                LabWorkFilterConfiguration labWorkFilterConfiguration = parseFilterRequest(request);
+                List<LabWork> listLabWork = labWorkService.findAllLabWorks(labWorkFilterConfiguration);
 
-            log.info(request.getQueryString());
+                log.info(request.getQueryString());
 
-            sendOkResponse(response, jsonUtilLabWork.buildJsonStringFromList(listLabWork));
+                sendResponse(response, jsonUtilLabWork.buildJsonStringFromList(listLabWork), HttpServletResponse.SC_OK);
+            } catch (IncorrectDataException e) {
+                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
+            }
         } else if (path.equals("/difficulty/count")) {
             // Get count of LabWork which has difficulty more than
             try {
                 String diff = request.getParameter("difficulty");
                 int count = labWorkService.getCountLabWorkByDifficulty(diff);
-                sendOkResponse(response, jsonUtilString.buildJsonStringFromObject(String.format("%s: %d", diff, count)));
+                sendResponse(response, jsonUtilString.buildJsonStringFromObject(String.format("%s: %d", diff, count)), HttpServletResponse.SC_OK);
             } catch (IncorrectDataException e) {
-                sendErrorListResponse(response, e.getErrorList());
+                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
             }
         } else if (path.equals("/name/substr")) {
             // Get all LabWorks which contains name
             List<LabWork> listLabWork = labWorkService.findAllLabWorkByName(request.getParameter("name_substr"));
-            sendOkResponse(response, jsonUtilLabWork.buildJsonStringFromList(listLabWork));
+            sendResponse(response, jsonUtilLabWork.buildJsonStringFromList(listLabWork), HttpServletResponse.SC_OK);
         } else {
             path = path.replaceAll("/","");
             try {
                 LabWork labWork = labWorkService.findLabWorkById(Integer.parseInt(path));
-                sendOkResponse(response,jsonUtilLabWork.buildJsonStringFromObject(labWork));
+                sendResponse(response,jsonUtilLabWork.buildJsonStringFromObject(labWork), HttpServletResponse.SC_OK);
             } catch (NumberFormatException e) {
-                sendErrorListResponse(response, Collections.singletonList(new IntegrityError(404,"Incorrect path")));
+                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
+                        new IntegrityError(404,"Resource not found")), HttpServletResponse.SC_NOT_FOUND);
             } catch (IncorrectDataException e) {
-                sendErrorListResponse(response, e.getErrorList());
+                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_NOT_FOUND);
             }
-        }
-
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        log.info("PUT");
-
-        // Get LabWork object from HttpServletRequest
-        LabWork labWork = jsonUtilLabWork.buildObjectFromRequest(request);
-        log.info(labWork.toString());
-
-        // Update LabWork
-        try {
-            labWorkService.updateLabWork(labWork);
-        } catch (IncorrectDataException e) {
-            sendErrorListResponse(response, e.getErrorList());
         }
     }
 
@@ -97,7 +85,38 @@ public class LabWorkServlet extends HttpServlet {
         try {
             labWorkService.createLabWork(labWork);
         } catch (IncorrectDataException e) {
-            sendErrorListResponse(response, e.getErrorList());
+            sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("PUT");
+
+        String path = request.getPathInfo();
+
+        if ((path != null)) {
+            path = path.replaceAll("/","");
+
+            // Update LabWork
+            try {
+                LabWork labWork = jsonUtilLabWork.buildObjectFromRequest(request);
+                labWork.setId(labWorkService.findLabWorkById(Integer.parseInt(path)).getId());
+                log.info(labWork.toString());
+                labWorkService.updateLabWork(labWork);
+            } catch (NumberFormatException e) {
+                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
+                        new IntegrityError(404,"Resource not found")), HttpServletResponse.SC_NOT_FOUND);
+            } catch (IncorrectDataException e) {
+                if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
+                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_NOT_FOUND);
+                } else {
+                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
+                }
+            }
+        } else {
+            sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
+                    new IntegrityError(404, "Resource not found")), HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
@@ -106,7 +125,7 @@ public class LabWorkServlet extends HttpServlet {
         log.info("DELETE");
 
         String path = request.getPathInfo();
-        log.info(request.getQueryString());
+        log.info(path);
 
         if (path != null) {
             if (path.equals("/difficulty")) {
@@ -114,38 +133,40 @@ public class LabWorkServlet extends HttpServlet {
 
                 // Delete one LabWork by difficulty
                 try {
-                    labWorkService.deleteLabWorkByDifficulty(request.getParameter("difficulty"));
+                    LabWork labWork = labWorkService.deleteLabWorkByDifficulty(request.getParameter("difficulty"));
+                    sendResponse(response, jsonUtilLabWork.buildJsonStringFromObject(labWork), HttpServletResponse.SC_OK);
                 } catch (IncorrectDataException e) {
-                    sendErrorListResponse(response, e.getErrorList());
+                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
                 }
             } else {
                 // Delete LabWork by id
                 path = path.replaceAll("/","");
+
                 try {
-                    labWorkService.deleteLabWork(Integer.parseInt(path));
+                    LabWork labWork = labWorkService.findLabWorkById(Integer.parseInt(path));
+                    labWorkService.deleteLabWork(labWork);
+                    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 } catch (NumberFormatException e) {
-                    sendErrorListResponse(response, Collections.singletonList(new IntegrityError(404,"Incorrect path")));
+                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(new IntegrityError(404,"Resource not found")), HttpServletResponse.SC_NOT_FOUND);
                 } catch (IncorrectDataException e) {
-                    sendErrorListResponse(response, e.getErrorList());
+                    if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
+                        sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_NOT_FOUND);
+                    } else {
+                        sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
+                    }
                 }
             }
+        } else {
+            sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
+                    new IntegrityError(404, "Resource not found")), HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
-    private void sendErrorListResponse(HttpServletResponse response, List<IntegrityError> errorList) throws IOException {
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(errorList));
-    }
-
-    private void sendOkResponse(HttpServletResponse response, String responseString) throws IOException {
-        response.setStatus(HttpServletResponse.SC_OK);
-        sendResponse(response, responseString);
-    }
-
-    private void sendResponse(HttpServletResponse response, String responseString) throws IOException {
+    private void sendResponse(HttpServletResponse response, String responseString, int statusCode) throws IOException {
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+        response.setStatus(statusCode);
         out.print(responseString);
         out.flush();
     }
