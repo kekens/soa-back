a1b77727fe060641f466551e66c13679b269b4b7
kirill <kirillbobryakov82@gmail.com>
2021-10-13 17:01:28 +0300

    fix: add a check to see if a given discipline exists

--- src/main/java/com/kekens/soa_lab_1/validator/LabWorkValidator.java
+++ src/main/java/com/kekens/soa_lab_1/validator/LabWorkValidator.java
@@ -1,5 +1,7 @@
 package com.kekens.soa_lab_1.validator;
 
+import com.kekens.soa_lab_1.dao.DisciplineDao;
+import com.kekens.soa_lab_1.model.Discipline;
 import com.kekens.soa_lab_1.model.LabWork;
 
 import java.util.ArrayList;
@@ -11,6 +13,7 @@ public class LabWorkValidator {
         List<IntegrityError> errorList = new ArrayList<>();
         CoordinatesValidator coordinatesValidator = new CoordinatesValidator();
         DisciplineValidator disciplineValidator = new DisciplineValidator();
+        DisciplineDao disciplineDao = new DisciplineDao();
 
         if ((labWork.getName() == null) || (labWork.getName().isEmpty())) {
             errorList.add(new IntegrityError(400, "LabWork name mustn't be empty"));
@@ -18,6 +21,8 @@ public class LabWorkValidator {
 
         if ((labWork.getCoordinates() == null)) {
             errorList.add(new IntegrityError(400, "LabWork coordinates mustn't be empty"));
+        } else {
+            errorList.addAll(coordinatesValidator.validateCoordinates(labWork.getCoordinates()));
         }
 
         if ((labWork.getMinimalPoint() != null) && (labWork.getMinimalPoint() <= 0)) {
@@ -26,10 +31,22 @@ public class LabWorkValidator {
 
         if ((labWork.getDiscipline() == null)) {
             errorList.add(new IntegrityError(400, "LabWork discipline mustn't be empty"));
+        } else {
+            errorList.addAll(disciplineValidator.validateDiscipline(labWork.getDiscipline()));
+
+            int id = labWork.getDiscipline().getId();
+
+            if (id < 1) {
+                errorList.add(new IntegrityError(400, String.format("Incorrect value '%d' of discipline ID", id)));
+            }
+
+            Discipline discipline = disciplineDao.findById(id);
+
+            if ((discipline == null) || (!discipline.equals(labWork.getDiscipline()))) {
+                errorList.add(new IntegrityError(400, String.format("Cannot find this discipline: %s", labWork.getDiscipline().toString())));
+            }
         }
 
-        errorList.addAll(coordinatesValidator.validateCoordinates(labWork.getCoordinates()));
-        errorList.addAll(disciplineValidator.validateDiscipline(labWork.getDiscipline()));
 
         return errorList;
     }
