3e2112d71b1146a62fbc0291649bfd6387650d2f
kirill <kirillbobryakov82@gmail.com>
2021-11-01 22:55:47 +0300

    refactor: change custom JSON to JAX's

--- soa-back-1/src/main/java/com/kekens/soa_back/resource/DisciplineResource.java
+++ soa-back-1/src/main/java/com/kekens/soa_back/resource/DisciplineResource.java
@@ -1,10 +1,8 @@
 package com.kekens.soa_back.resource;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kekens.soa_back.model.Discipline;
 import com.kekens.soa_back.service.DisciplineService;
 import com.kekens.soa_back.service.impl.DisciplineServiceImpl;
-import com.kekens.soa_back.util.JsonUtil;
 import com.kekens.soa_back.validator.IntegrityError;
 import com.kekens.soa_back.validator.exception.IncorrectDataException;
 
@@ -20,8 +18,6 @@ import java.util.stream.Collectors;
 public class DisciplineResource {
 
     DisciplineService disciplineService = new DisciplineServiceImpl();
-    private final JsonUtil<Discipline> jsonUtilDiscipline = new JsonUtil<>(Discipline.class);
-    private final JsonUtil<IntegrityError> jsonUtilIntegrityError = new JsonUtil<>(IntegrityError.class);
 
     @GET
     public Response getAllDisciplines() {
@@ -31,36 +27,34 @@ public class DisciplineResource {
 
     @GET
     @Path("/{id}")
-    public Response getDisciplineById(@PathParam("id") String id) throws JsonProcessingException {
+    public Response getDisciplineById(@PathParam("id") String id) {
         try {
             Discipline discipline = disciplineService.findDisciplineById(Integer.parseInt(id));
-            return Response.status(Response.Status.OK).entity(jsonUtilDiscipline.buildJsonStringFromObject(discipline)).build();
+            return Response.status(Response.Status.OK).entity(discipline).build();
         } catch (NumberFormatException | IncorrectDataException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404,"Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404,"Resource not found")).build();
         }
     }
 
     @POST
-    public Response createDiscipline(Discipline discipline) throws JsonProcessingException {
+    public Response createDiscipline(Discipline discipline) {
         try {
             disciplineService.createDiscipline(discipline);
             return Response.status(Response.Status.OK).build();
         } catch (IncorrectDataException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity( e.getErrorList()).build();
         }
     }
 
     @PUT
     @Path("/{id}")
-    public Response updateDiscipline(@PathParam("id") String id, Discipline discipline) throws JsonProcessingException {
+    public Response updateDiscipline(@PathParam("id") String id, Discipline discipline) {
         try {
             discipline.setId(disciplineService.findDisciplineById(Integer.parseInt(id)).getId());
             disciplineService.updateDiscipline(discipline);
             return Response.status(Response.Status.OK).build();
         } catch (NumberFormatException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404, "Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404, "Resource not found")).build();
         } catch (IncorrectDataException e) {
             Response.Status status;
             if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
@@ -69,20 +63,19 @@ public class DisciplineResource {
                 status = Response.Status.BAD_REQUEST;
             }
 
-            return Response.status(status).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(status).entity( e.getErrorList()).build();
         }
     }
 
     @DELETE
     @Path("/{id}")
-    public Response deleteDiscipline(@PathParam("id") String id) throws JsonProcessingException {
+    public Response deleteDiscipline(@PathParam("id") String id) {
         try {
             Discipline discipline = disciplineService.findDisciplineById(Integer.parseInt(id));
             disciplineService.deleteDiscipline(discipline);
             return Response.status(Response.Status.NO_CONTENT).build();
         } catch (NumberFormatException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404, "Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404, "Resource not found")).build();
         } catch (IncorrectDataException e) {
             Response.Status status;
             if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
@@ -91,35 +84,8 @@ public class DisciplineResource {
                 status = Response.Status.BAD_REQUEST;
             }
 
-            return Response.status(status).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(status).entity( e.getErrorList()).build();
         }
     }
-
-//    @Override
-//    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String path = request.getPathInfo();
-//
-//        if (path != null) {
-//            path = path.replaceAll("/", "");
-//
-//            try {
-//                Discipline discipline = disciplineService.findDisciplineById(Integer.parseInt(path));
-//                disciplineService.deleteDiscipline(discipline);
-//                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-//            } catch (NumberFormatException e) {
-//                sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
-//                        new IntegrityError(404,"Resource not found")), HttpServletResponse.SC_NOT_FOUND);
-//            } catch (IncorrectDataException e) {
-//                if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
-//                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_NOT_FOUND);
-//                } else {
-//                    sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList()), HttpServletResponse.SC_BAD_REQUEST);
-//                }
-//            }
-//        } else {
-//            sendResponse(response, jsonUtilIntegrityError.buildJsonStringFromObject(
-//                    new IntegrityError(404, "Resource not found")), HttpServletResponse.SC_NOT_FOUND);
-//        }
-//    }
 
 }
--- soa-back-1/src/main/java/com/kekens/soa_back/resource/LabWorkResource.java
+++ soa-back-1/src/main/java/com/kekens/soa_back/resource/LabWorkResource.java
@@ -1,11 +1,9 @@
 package com.kekens.soa_back.resource;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kekens.soa_back.model.LabWork;
 import com.kekens.soa_back.service.LabWorkService;
 import com.kekens.soa_back.service.impl.LabWorkServiceImpl;
 import com.kekens.soa_back.util.LabWorkFilterConfiguration;
-import com.kekens.soa_back.util.JsonUtil;
 import com.kekens.soa_back.validator.IntegrityError;
 import com.kekens.soa_back.validator.exception.IncorrectDataException;
 
@@ -14,7 +12,6 @@ import javax.ws.rs.core.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Path("/labworks")
@@ -23,19 +20,14 @@ import java.util.stream.Collectors;
 public class LabWorkResource {
 
     private final LabWorkService labWorkService = new LabWorkServiceImpl();
-    private final JsonUtil<LabWork> jsonUtilLabWork = new JsonUtil<>(LabWork.class);
-    private final JsonUtil<IntegrityError> jsonUtilIntegrityError = new JsonUtil<>(IntegrityError.class);
-    private final JsonUtil<String> jsonUtilString = new JsonUtil<>(String.class);
-
-    static Logger log = Logger.getLogger(LabWorkResource.class.getName());
 
     @GET
-    public Response getAllLabWorks(@Context UriInfo uriInfo) throws JsonProcessingException {
+    public Response getAllLabWorks(@Context UriInfo uriInfo)  {
         try {
             LabWorkFilterConfiguration labWorkFilterConfiguration = parseFilterRequest(uriInfo.getQueryParameters());
             List<LabWork> listLabWork = labWorkService.findAllLabWorks(labWorkFilterConfiguration);
 
-            return Response.status(Response.Status.OK).entity(jsonUtilLabWork.buildJsonStringFromList(listLabWork)).build();
+            return Response.status(Response.Status.OK).entity(listLabWork).build();
         } catch (IncorrectDataException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getErrorList()).build();
         }
@@ -43,50 +35,53 @@ public class LabWorkResource {
 
     @GET
     @Path("/{id}")
-    public Response getLabWork(@PathParam("id") String id) throws JsonProcessingException {
+    public Response getLabWork(@PathParam("id") String id) {
         try {
             LabWork labWork = labWorkService.findLabWorkById(Integer.parseInt(id));
-            return Response.status(Response.Status.OK).entity(jsonUtilLabWork.buildJsonStringFromObject(labWork)).build();
+            return Response.status(Response.Status.OK).entity(labWork).build();
         } catch (NumberFormatException | IncorrectDataException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404, "Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404, "Resource not found")).build();
         }
     }
 
     @GET
     @Path("/difficulty/count")
-    public Response getCountByDifficultyMore(@QueryParam("difficulty") String difficulty) throws JsonProcessingException {
+    public Response getCountByDifficultyMore(@QueryParam("difficulty") String difficulty) {
         try {
             int count = labWorkService.getCountLabWorkByDifficulty(difficulty);
-            return Response.status(Response.Status.OK).entity(
-                    jsonUtilString.buildJsonStringFromObject(String.format("%s: %d", difficulty, count))).build();
+            return Response.status(Response.Status.OK).entity(String.format("%s: %d", difficulty, count)).build();
         } catch (IncorrectDataException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getErrorList()).build();
         }
     }
 
     @POST
-    public Response createLabWork(LabWork labWork) throws JsonProcessingException {
+    public Response createLabWork(LabWork labWork) {
         try {
+            System.out.println("\n\n");
+            System.out.println("AAAAA");
+            System.out.println("\n\n");
             labWorkService.createLabWork(labWork);
 
             return Response.status(Response.Status.OK).build();
         } catch (IncorrectDataException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getErrorList()).build();
         }
     }
 
     @PUT
     @Path("/{id}")
-    public Response updateLabWork(@PathParam("id") String id, LabWork labWork) throws JsonProcessingException {
+    public Response updateLabWork(@PathParam("id") String id, LabWork labWork) {
         try {
+            System.out.println("\n\n");
+            System.out.println(labWork);
+            System.out.println("\n\n");
             labWork.setId(labWorkService.findLabWorkById(Integer.parseInt(id)).getId());
             labWorkService.updateLabWork(labWork);
 
             return Response.status(Response.Status.OK).build();
         } catch (NumberFormatException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404, "Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404, "Resource not found")).build();
         } catch (IncorrectDataException e) {
             Response.Status status;
             if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
@@ -94,21 +89,20 @@ public class LabWorkResource {
             } else {
                 status = Response.Status.BAD_REQUEST;
             }
-            return Response.status(status).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(status).entity(e.getErrorList()).build();
         }
     }
 
     @DELETE
     @Path("/{id}")
-    public Response deleteLabWork(@PathParam("id") String id) throws JsonProcessingException {
+    public Response deleteLabWork(@PathParam("id") String id) {
         try {
             LabWork labWork = labWorkService.findLabWorkById(Integer.parseInt(id));
             labWorkService.deleteLabWork(labWork);
 
             return Response.status(Response.Status.NO_CONTENT).build();
         } catch (NumberFormatException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(jsonUtilIntegrityError.buildJsonStringFromObject(
-                    new IntegrityError(404, "Resource not found"))).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(new IntegrityError(404, "Resource not found")).build();
         } catch (IncorrectDataException e) {
             Response.Status status;
             if (e.getErrorList().stream().map(IntegrityError::getCode).collect(Collectors.toList()).contains(404)) {
@@ -117,18 +111,18 @@ public class LabWorkResource {
                 status = Response.Status.BAD_REQUEST;
             }
 
-            return Response.status(status).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(status).entity(e.getErrorList()).build();
         }
     }
 
     @DELETE
     @Path("/difficulty")
-    public Response deleteByDifficulty(@QueryParam("difficulty") String difficulty) throws JsonProcessingException {
+    public Response deleteByDifficulty(@QueryParam("difficulty") String difficulty) {
         try {
             LabWork labWork = labWorkService.deleteLabWorkByDifficulty(difficulty);
-            return Response.status(Response.Status.OK).entity(jsonUtilLabWork.buildJsonStringFromObject(labWork)).build();
+            return Response.status(Response.Status.OK).entity(labWork).build();
         } catch (IncorrectDataException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(jsonUtilIntegrityError.buildJsonStringFromList(e.getErrorList())).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getErrorList()).build();
         }
     }
 
